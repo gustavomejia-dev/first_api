3e103934d27aa13a4e83a7ac227250fcccba9a7f
gustavomejia-dev <ksks_mejia@hotmail.com>
2022-12-05 08:29:46 -0300

    api_first_commit

---
+++ userdept/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('grupo_teste1');
+INSERT INTO tb_department(name) VALUES ('grupo_teste2');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'teste1', 'teste1@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Teste2', 'teste2@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Test3', 'teste3@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'teste4', 'teste4@gmail.com');
